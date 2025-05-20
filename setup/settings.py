b531f7fa8ffbad8ef919f411a5883709e336a0a9
PaulaFCBR <paulaweb@yahoo.com.br>
2025-05-19 21:43:06 -0300

    static

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,6 +126,7 @@ STATIC_URL = 'static/'
 STATIC_DIRS = [
     BASE_DIR / STATIC_URL,
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
