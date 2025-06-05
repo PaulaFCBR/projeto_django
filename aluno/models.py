6f27053a37823c6cde9a4f2614a3accfb54ec4e1
PaulaFCBR <paulaweb@yahoo.com.br>
2025-06-04 21:36:58 -0300

    aluno models

--- aluno/models.py
+++ aluno/models.py
@@ -5,7 +5,10 @@ class Aluno(models.Model):
     matricula = models.AutoField(primary_key=True, help_text='Entre com a Matricula do Aluno')
     nome = models.CharField(max_length=100, help_text='Informe p nome do Aluno')
     data_inicial = models.DateField('Informe a data de Matricula')
-    data_fim = models.DateField('Informe Data final do Aluno')
+    data_fim = models.DateField(null=True,
+                                blank=True,
+                                default=timezone.now(),
+                                help_text='Informe Data final do Aluno')
     def __str__(self):
         return f'{self.matricula} - {self.nome} - {self.data_inicial} - {self.data_fim}'  
 
