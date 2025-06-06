7ee59178fc87715c0960cf0828963926c6690baf
PaulaFCBR <paulaweb@yahoo.com.br>
2025-06-06 20:02:52 -0300

    tabelas titulo instrutor

--- aluno/models.py
+++ aluno/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Aluno(models.Model):
     matricula = models.AutoField(primary_key=True, help_text='Entre com a Matricula do Aluno')
     nome = models.CharField(max_length=100, help_text='Informe p nome do Aluno')
-    data_inicial = models.DateField('Informe a data de Matricula')
+    data_inicial = models.DateField(null=False, default=timezone.now(),
+                                    help_text='Informe a data de Matricula na escola')
     data_fim = models.DateField(null=True,
                                 blank=True,
-                                default=timezone.now(),
-                                help_text='Informe Data final do Aluno')
+                                help_text='Informe Data final do Aluno na escola')
     def __str__(self):
         return f'{self.matricula} - {self.nome} - {self.data_inicial} - {self.data_fim}'  
 
--- instrutor/models.py
+++ instrutor/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from django.utils import timezone
+
+from titulo.models import Titulo
 
 # Create your models here.
 class Instrutor(models.Model):
     id = models.AutoField(primary_key=True, help_text='ID do instrutor')
     rg = models.CharField(max_length=15, help_text='Informe o RG do Instrutor')
     nome = models.CharField(max_length=100, help_text='Informe p nome do Instrutor')
-    data_nascimento = models.DateField(help_text='Informe a data de nascimento')
+    data_nascimento = models.DateField(null=True,
+                                       blank=True,
+                                       default=timezone.now(),
+                                       help_text='Informe a data de nascimento')
     telefone = models.CharField(max_length=9, help_text='Informe o telefone do Instrutor')
     ddd = models.CharField(max_length=3, help_text='Informe o DDD do Instrutor')
+
+    codigo_titulo = models.ForeignKey(Titulo, null=True,
+                                      related_name='titulos',
+                                      on_delete=models.SET_NULL,
+                                      db_column=' titulo_codigo')
     
     def __str__(self):
         return f'{self.id} - {self.nome} - {self.rg} - {self.data_nascimento} - {self.telefone} - {self.ddd}' 
